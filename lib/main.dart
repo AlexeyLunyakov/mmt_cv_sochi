37938b39e64318f2a62117365dd092911d801448
Alexey Lunyakov <a.lunyakov@my.com>
2023-10-08 16:43:18 +0300

    post image test

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'MMT_CV',
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      //   useMaterial3: true,
-      // ),
       home: WelcomeScreen(),
     );
   }
